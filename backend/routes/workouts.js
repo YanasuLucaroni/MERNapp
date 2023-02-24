0dd04f02e427bc44b8d739db9e4144cd62805689
YanasuLucaroniW10 <81329768+YanasuLucaroni@users.noreply.github.com>
2023-02-24 15:54:29 -0300

    Terminado el segundo video de controladores, ya se pueden eliminar y updatear workouts

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -4,6 +4,8 @@ const {
   createWorkout,
   getWorkouts,
   getWorkout,
+  deleteWorkout,
+  updateWorkout,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
@@ -18,13 +20,9 @@ router.get("/:id", getWorkout);
 router.post("/", createWorkout);
 
 // DELETE a new workout
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 // UPDATE a new workout
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "UPDATE a workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
